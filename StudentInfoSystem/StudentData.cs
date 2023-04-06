9a45d7e04ba12bcd62e4042e87e1e5bb7f8aacd9
PetkoLuyts <simona2000petko@abv.bg>
2023-04-06 12:42:15 +0300

    Implement StudentData class

---
+++ StudentInfoSystem/StudentData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInfoSystem
+{
+    static class StudentData
+    {
+        public static List<Student> TestStudents
+        {
+            get
+            {
+                Student student = new Student();
+                student.FirstName = "Petko";
+                student.MiddleName = "Delyanov";
+                student.LastName = "Lyutskanov";
+                student.Faculty = "FCST";
+                student.Speciality = "CSE";
+                student.Degree = "bachelor";
+                student.Status = "paid";
+                student.FacultyNumber = "121220186";
+                student.Course = 3;
+                student.Stream = 10;
+                student.GroupNumber = 44;
+
+                return new List<Student>() { student };
+            }
+            private set { }
+        }
+    }
+}
